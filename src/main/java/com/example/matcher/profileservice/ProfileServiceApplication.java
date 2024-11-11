9e182eeda336b9456f480332710e8ba47a21648a
Daniil Voropaev <vorop2004@mail.ru>
2024-11-11 19:25:52 +0300

    v1.0.1 Move sensitive data to .env file

--- src/main/java/com/example/matcher/profileservice/ProfileServiceApplication.java
+++ src/main/java/com/example/matcher/profileservice/ProfileServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example.matcher.profileservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +13,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 //@EnableDiscoveryClient
 public class ProfileServiceApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // Загружаем переменные из .env
+		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("token.signing.key", dotenv.get("TOKEN_SIGNING_KEY"));
+		System.setProperty("aws.access.key", dotenv.get("AWS_ACCESS_KEY"));
+		System.setProperty("aws.secret.key", dotenv.get("AWS_SECRET_KEY"));
 		SpringApplication.run(ProfileServiceApplication.class, args);
 	}
 
