519f841c9262c4eef9ddcfefa34b5f1cf36e70df
Daniil Voropaev <vorop2004@mail.ru>
2025-01-16 00:40:11 +0300

    Add logic in Redis service for search and update online status

--- src/main/java/com/example/matcher/profileservice/model/StatusConnectionUpdate.java
+++ src/main/java/com/example/matcher/profileservice/model/StatusConnectionUpdate.java
@@ -2,11 +2,13 @@ package com.example.matcher.profileservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class StatusConnectionUpdate {
     UUID userId;
     StatusConnection statusConnection;
--- src/main/java/com/example/matcher/profileservice/service/StatusConnectionService.java
+++ src/main/java/com/example/matcher/profileservice/service/StatusConnectionService.java
@@ -5,6 +5,9 @@ import com.example.matcher.profileservice.model.StatusConnectionUpdate;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class StatusConnectionService {
@@ -12,10 +15,14 @@ public class StatusConnectionService {
     private final RedisService redisService;
     public void statusConnectionUpdate(StatusConnectionUpdate newStatus) {
         if (StatusConnection.ONLINE.equals(newStatus.getStatusConnection())) {
-            redisService.save("user_status:" + newStatus.getUserId().toString(), newStatus);
+            redisService.save("user_status:" + newStatus.getUserId().toString(), newStatus, Duration.ofMinutes(5));
         }
         else {
             redisService.delete("user_status:" + newStatus.getUserId().toString());
         }
+    }
+
+    public List<StatusConnectionUpdate> getAllOnlineUsers() {
+        return redisService.getAllOnlineUsers();
     }
 }
