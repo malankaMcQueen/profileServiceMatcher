85374e5189017dd8c1c42c34929c81f59ae5d2ce
Daniil Voropaev <vorop2004@mail.ru>
2025-02-14 23:12:56 +0300

    tmp commit

--- src/main/java/com/example/matcher/profileservice/dto/ProfileCreateDTO.java
+++ src/main/java/com/example/matcher/profileservice/dto/ProfileCreateDTO.java
@@ -1,6 +1,7 @@
 package com.example.matcher.profileservice.dto;
 
 import com.example.matcher.profileservice.model.Gender;
+import com.example.matcher.profileservice.model.Profile;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -17,4 +18,18 @@ public class ProfileCreateDTO {
     private String email;
     private String university;
     private String faculty;
+
+    public static Profile profileFromDTO(ProfileCreateDTO profileCreateDTO) {
+        return Profile.builder()
+                .firstName(profileCreateDTO.getFirstName())
+                .city(profileCreateDTO.getCity())
+                .dateOfBirth(profileCreateDTO.getDateOfBirth())
+                .gender(profileCreateDTO.getGender())
+                .email(profileCreateDTO.getEmail())
+                .university(profileCreateDTO.getUniversity())
+                .faculty(profileCreateDTO.getFaculty())
+                .build();
+    }
 }
+
+
