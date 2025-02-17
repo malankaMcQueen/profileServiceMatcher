c91f8e7fd26d08a4acaf6c1613a189bc8b5d6dfa
Daniil Voropaev <vorop2004@mail.ru>
2025-02-17 16:20:10 +0300

    Add swagger documentation

--- src/main/java/com/example/matcher/profileservice/controllers/ProfileController.java
+++ src/main/java/com/example/matcher/profileservice/controllers/ProfileController.java
@@ -7,6 +7,10 @@ import com.example.matcher.profileservice.model.StatusConnection;
 import com.example.matcher.profileservice.model.StatusConnectionUpdate;
 import com.example.matcher.profileservice.service.ProfileService;
 import com.example.matcher.profileservice.service.StatusConnectionService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,13 +30,21 @@ public class ProfileController {
     private final ProfileService profileService;
     private final StatusConnectionService statusConnectionService;
 
+    @Operation(summary = "Создание профиля",
+            description = "Метод создания профиля")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
+    @ApiResponse(responseCode = "409", description = "Профиль с таким UUID существует", content = @Content())
     @PostMapping("/create")
     public ResponseEntity<Profile> createProfile(@RequestBody ProfileCreateDTO profile, @RequestParam UUID userId) {
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 //        UUID userId = UUID.fromString((String) authentication.getPrincipal()); // UUID будет в качестве principal
         return new ResponseEntity<>(profileService.createProfile(profile, userId), HttpStatus.OK);
     }
-
+    @Operation(summary = "Обновить профиль",
+            description = "Метод обновляет данные о профиле")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
+    @ApiResponse(responseCode = "404", description = "Профиль не найден", content = @Content())
+    @ApiResponse(responseCode = "400", description = "Неправильный запрос (тело запроса пустое/обновлений нет)", content = @Content())
     @PatchMapping("/updateProfile")
     public ResponseEntity<Profile> updateProfile(@RequestBody ProfileUpdateDTO profile, @RequestParam UUID userId) {
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -40,6 +52,9 @@ public class ProfileController {
         return new ResponseEntity<>(profileService.updateProfile(userId, profile), HttpStatus.OK);
     }
 
+    @Operation(summary = "Удалить профиль",
+            description = "Метод удаляет профиль")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
     @Transactional
     @DeleteMapping("/delete")
     public ResponseEntity<?> deleteProfile(@RequestParam UUID userId) {
@@ -48,33 +63,51 @@ public class ProfileController {
         return new ResponseEntity<>(profileService.deleteProfile(userId), HttpStatus.OK);
     }
 
+    @Operation(summary = "Получить все существующие профили",
+            description = "")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
     @GetMapping()
     public ResponseEntity<List<Profile>> getAllProfiles() {
         return new ResponseEntity<>(profileService.getAllProfiles(), HttpStatus.OK);
     }
 
+    @Hidden
     @GetMapping("/statusUsers")
     public ResponseEntity<List<StatusConnectionUpdate>> getUserStatus() {
         return new ResponseEntity<>(statusConnectionService.getAllOnlineUsers(), HttpStatus.OK);
     }
+
+    @Hidden
     @PostMapping("/statusUsers/update")
     public void updateConnectionStatus(@RequestBody StatusConnectionUpdate statusConnectionUpdate) {
         statusConnectionService.statusConnectionUpdate(statusConnectionUpdate);
         return;
     }
 
-    @GetMapping("/myProfile")
-    public ResponseEntity<Profile> getMyProfile(@RequestParam UUID userId) {
+    @Operation(summary = "Получить профиль с UUID:",
+            description = "")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
+    @GetMapping("/{userId}")
+    public ResponseEntity<Profile> getProfile(@PathVariable UUID userId) {
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 //        UUID userId = UUID.fromString((String) authentication.getPrincipal()); // UUID будет в качестве principal
         return new ResponseEntity<>(profileService.getProfileByUserId(userId), HttpStatus.OK);
     }
 
+    @Operation(summary = "Добавить фото в профиль",
+            description = "")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
+    @ApiResponse(responseCode = "404", description = "Профиль не найден")
+    @ApiResponse(responseCode = "400", description = "Лимит по фото достигнут")
     @PostMapping("/updateProfile/addPhoto")
     public ResponseEntity<List<String>> addPhotoInProfile(@RequestParam UUID userId, @RequestParam("file") MultipartFile file) throws IOException{
         return new ResponseEntity<>(profileService.addPhotoInProfile(userId, file), HttpStatus.OK);
     }
 
+    @Operation(summary = "Удалить фото из профиля",
+            description = "")
+    @ApiResponse(responseCode = "200", description = "Успешный ответ")
+    @ApiResponse(responseCode = "404", description = "Профиль не найден/Фото с данным id не найдено")
     @DeleteMapping("/updateProfile/deletePhoto")
     public ResponseEntity<List<String>> deletePhotoInProfile(@RequestParam UUID userId, String link) {
         return new ResponseEntity<>(profileService.deletePhotoInProfile(userId, link), HttpStatus.OK);
